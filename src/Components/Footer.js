e936a6068c42dad55b744d560af07b4cb6b685cf
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-05-25 15:52:50 +0300

    add a footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,23 +1,62 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import '../Styling/Footer.css'
+import logistics from '../assets/logistics.png'
+import location from '../assets/location.png'
+import email from '../assets/email.png'
+import telephone from '../assets/telephone.png'
+
+
 
 const Footer = () => {
   return (
     <Container fluid className='bg-black mt-4'>
-        <Row>
-            <Col>
+        <Row >
 
+            <Col className='mt-5'>
+            <Row className='d-flex justify-content-center'>
+                <img className='w-25' src={logistics} alt='Logo'/>
+            </Row>
+            <h4 className='logo-name text-center'>SamWest Distributors</h4>
             </Col>
 
-            <Col>
+
+            <Col className='mt-5'>
+            <h4 className='contacts-info'>Useful Links</h4>
+            <ul>
+                <li className='contacts-info'>Home</li>
+                <li className='contacts-info'>Shop</li>
+                <li className='contacts-info'>Login/Register</li>
+
+            </ul>
             
             </Col>
 
-            <Col>
-            
+            <Col className='mt-5'>
+                <h4 className='contacts-info'>Customer Service</h4>
+                <p className='contacts-info'>Our customer Service team is available to help you </p>
+                <p className='contacts-info'>Monday - Friday :8am to 5pm </p>
+        
             </Col>
 
-            <Col>
+            <Col className='mt-5'>
+            <h4 className='contacts-info'>Contacts & Location </h4>
+            <Row className='d-flex '>
+                {/* <img className='contacts-logo' src={location} alt='location'/> */}
+                <p className='contacts-info'>Nairobi ,Kenya</p>
+            </Row>
+
+            <Row className='d-flex '>
+                {/* <img className='contacts-logo' src={telephone} alt='location'/> */}
+                <p className='contacts-info'>0712 345 678</p>
+            </Row>
+
+
+            <Row className='d-flex '>
+                {/* <img className='contacts-logo' src={email} alt='location'/> */}
+                <p className='contacts-info'>samdistributors.co.ke</p>
+            </Row>
+            
             
             </Col>
         </Row>
@@ -28,7 +67,7 @@ const Footer = () => {
 
     {/* Credits section  */}
         <Row>
-            <p className='credits text-center'>Designed and Made By: <a href='https://www.linkedin.com/in/geoffrey-gichaga-234318ba/' target='_blank'>Geoffrey Gichaga.G</a></p>
+            <p className='credits text-center'>Designed and Made By: <a href='https://www.linkedin.com/in/geoffrey-gichaga-234318ba/' target='_blank' rel="noreferrer"><span>Geoffrey Gichaga.G</span></a></p>
 
         </Row>
     </Container>
