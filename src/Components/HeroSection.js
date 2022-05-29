5c3057359531cedd94a0e53d9a107782ebbe8648
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-05-30 02:05:41 +0300

    style the tabs nav on the homepage

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -6,27 +6,29 @@ const CarouselItem = ()=>{
     return(
         <Container className='mt-4'>
             <Row>
-                <Col>
-                <Nav variant="pills" className="flex-column">
-                    <Nav.Item>
+                <Col sm={12} md={6} lg={2} className='mt-4'>
+                <Nav  variant="pills" className="flex-column mb-3">
+                    <Nav.Item className='tabs-nav mb-4'>
                         <Nav.Link id='tablink' eventKey="first">Food</Nav.Link>
                     </Nav.Item>
-                    <Nav.Item>
+                    <Nav.Item className='tabs-nav mb-4'>
                         <Nav.Link id='tablink1' eventKey="second">Baby Care</Nav.Link>
                     </Nav.Item>
-                    <Nav.Item>
+                    <Nav.Item className='tabs-nav mb-4'>
                         <Nav.Link id='tablink2' eventKey="third">Detergents</Nav.Link>
                     </Nav.Item>
 
-                    <Nav.Item>
+                    <Nav.Item className='tabs-nav mb-4'>
                         <Nav.Link id='tablink3' eventKey="fourth">Stationeries</Nav.Link>
                     </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link id='tablink4' eventKey="fifth">Tab 5</Nav.Link>
+                    <Nav.Item className='tabs-nav mb-4'>
+                        <Nav.Link id='tablink4' eventKey="fifth">Animal Products</Nav.Link>
                     </Nav.Item>
                 </Nav>
                 </Col>
-                <Col>
+
+
+                <Col className='mt-4'>
                 hello2
                 </Col>
             </Row>
